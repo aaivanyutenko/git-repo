006fed2c78f6ad1cf675cfad97258b7d277dcce6
Anton Ivanyutenko <anton.ivanyutenko@gmail.com>
2011-10-03 00:50:20 +0300

    Adding class with unique constants

---
+++ diplom/src/by/gsu/mathan/data/OwnConstants.java
@@ -0,0 +1,6 @@
+package by.gsu.mathan.data;
+
+public class OwnConstants {
+	public static final String DEFINITIONS_ROOT_ID = "definition-root";
+	public static final String THEOREMS_ROOT_ID = "theorem-root";
+}
